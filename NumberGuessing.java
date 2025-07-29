4e5ee1681b6d8c23d4392acf31ae76642516eb50
Jashanuu <jsingh303@caledonian.ac.uk>
2025-07-29 01:31:02 +0000

    Add input validation to prevent crash on non-integer input

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,57 +1,53 @@
-import java.util.Scanner; 
- 
-public class NumberGuessing { 
-     
-    public static void guessingNumberGame() 
-    { 
-        Scanner sc = new Scanner(System.in); 
- 
-        // Generate a random number between 1 and 100 
-        int number = 1 + (int)(100 * Math.random()); 
-         
-        // Number of attempts 
-        int K = 5; 
- 
-        System.out.println( 
-            "A number is chosen between 1 and 100."); 
-        System.out.println( 
-            "You have " + K 
-            + " attempts to guess the correct number."); 
- 
-        // Loop for K attempts 
-        for (int i = 0; i < K; i++) { 
-            System.out.print("Enter your guess: "); 
-            int guess = sc.nextInt(); 
- 
-            // Check conditions 
-            if (guess == number) { 
-                System.out.println( 
-                    " Congratulations! You guessed the correct number."); 
-                sc.close(); 
-                 
-                // Exit function if guessed correctly 
-                return; 
-            } 
-            else if (guess < number) { 
-                System.out.println( 
-                    " The number is greater than " + guess); 
-            } 
-            else { 
-                System.out.println( 
-                    " The number is less than " + guess); 
-            } 
-        } 
- 
-        // If the user runs out of attempts 
-        System.out.println( 
-            "You've exhausted all attempts. The correct number was: " 
-            + number); 
-        sc.close(); 
-    } 
- 
-    public static void main(String[] args) 
-    { 
-        guessingNumberGame(); 
-    } 
-} 
- 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessing {
+
+    public static void guessingNumberGame() {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int numberToGuess = 1 + rand.nextInt(100);
+        int maxAttempts = 5;
+
+        System.out.println("A number is chosen between 1 and 100.");
+        System.out.println("You have " + maxAttempts + " attempts to guess the correct number.");
+
+        int i;
+        for (i = 0; i < maxAttempts; i++) {
+            System.out.print("Enter your guess: ");
+
+            int guess;
+
+            try {
+                guess = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                sc.next(); // Clear the invalid input
+                i--;       // Retry this attempt
+                continue;
+            }
+
+            if (guess == numberToGuess) {
+                System.out.println("Congratulations! You guessed the number.");
+                break;
+            } else if (guess > numberToGuess) {
+                System.out.println("The number is smaller than " + guess);
+            } else {
+                System.out.println("The number is greater than " + guess);
+            }
+        }
+
+        if (i == maxAttempts) {
+            System.out.println("You've used all attempts. The number was " + numberToGuess);
+        }
+
+        System.out.println("Bye-bye!");
+    }
+
+    public static void main(String[] args) {
+        guessingNumberGame();
+    }
+}
+
